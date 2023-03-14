6ddb8ee04f8e9edf2eb4be9f34d9b58e0f311938
Josh Brown <joshcarlbrown@gmail.com>
2023-03-14 22:22:34 +1100

    basic plotting

--- mandelbrot-rs/src/main.rs
+++ mandelbrot-rs/src/main.rs
@@ -1,12 +1,23 @@
+use anyhow::Result;
 use image::{Rgb, RgbImage};
+use mandelbrot_rs::mandelbrot::Complex;
 
 fn main() {
-    let mut img = RgbImage::new(32, 32);
-    for x in 15..=17 {
-        for y in 8..24 {
-            img.put_pixel(x, y, Rgb([255, 0, 0]));
-            img.put_pixel(y, x, Rgb([255, 0, 0]));
+    let mut img = RgbImage::new(4096, 4096);
+    for x in 0..img.width() {
+        for y in 0..img.width() {
+            let re = lerp(-2., 2., x as f64 / img.width() as f64);
+            let im = lerp(-2., 2., y as f64 / img.height() as f64);
+            let c = Complex::new(re, im);
+            let escape_count = c.escape_count(Complex::id(), 8., 200);
+            let frac = escape_count as f64 / 200. * 256.;
+            let col = Rgb([frac as u8, frac as u8, frac as u8]);
+            img.put_pixel(x, y, col);
         }
     }
     img.save("penis.png").unwrap();
+}
+
+fn lerp(start: f64, end: f64, frac: f64) -> f64 {
+    start + (end - start) * frac
 }
--- mandelbrot-rs/src/mandelbrot.rs
+++ mandelbrot-rs/src/mandelbrot.rs
@@ -1,1 +1,53 @@
+pub struct Complex {
+    re: f64,
+    im: f64,
+}
 
+impl ToString for Complex {
+    fn to_string(&self) -> String {
+        format!("{} + {}i", self.re, self.im)
+    }
+}
+
+impl Default for Complex {
+    fn default() -> Self {
+        Self { re: 0., im: 0. }
+    }
+}
+
+impl Complex {
+    pub fn new(re: f64, im: f64) -> Self {
+        Self { re, im }
+    }
+
+    pub fn id() -> Self {
+        Self::default()
+    }
+
+    pub fn mandelbrot_iter(&self, c: &Self) -> Self {
+        Self {
+            re: (self.re - self.im) * (self.re + self.im) + c.re,
+            im: 2. * self.re * self.im + c.im,
+        }
+    }
+
+    pub fn abs_value_sq(&self) -> f64 {
+        self.re * self.re + self.im * self.im
+    }
+
+    pub fn escape_count(&self, z_0: Self, bound: f64, max_iters: isize) -> isize {
+        if z_0.abs_value_sq() > bound {
+            return 0;
+        }
+        let bound_sq = bound.powf(2.);
+        let mut z_iter = z_0;
+        let iter: isize = 1;
+        for iter in 1..=max_iters {
+            z_iter = z_iter.mandelbrot_iter(self);
+            if z_iter.abs_value_sq() > bound_sq {
+                return iter;
+            }
+        }
+        return max_iters;
+    }
+}
