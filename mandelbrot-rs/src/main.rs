062f38119dc3cec9a5d1447b6794f38c8c89563b
Josh Brown <joshcarlbrown@gmail.com>
2023-04-24 16:00:22 +1000

    switch to for_each

--- mandelbrot-rs/src/main.rs
+++ mandelbrot-rs/src/main.rs
@@ -1,20 +1,25 @@
 use anyhow::Result;
-use image::{Rgb, RgbImage};
+use image::{ImageBuffer, Rgb, RgbImage};
 use mandelbrot_rs::mandelbrot::Complex;
 
 fn main() {
-    let mut img = RgbImage::new(4096, 4096);
-    for x in 0..img.width() {
-        for y in 0..img.width() {
-            let re = lerp(-2., 2., x as f64 / img.width() as f64);
-            let im = lerp(-2., 2., y as f64 / img.height() as f64);
-            let c = Complex::new(re, im);
-            let escape_count = c.escape_count(Complex::id(), 8., 200);
-            let frac = escape_count as f64 / 200. * 256.;
-            let col = Rgb([frac as u8, frac as u8, frac as u8]);
-            img.put_pixel(x, y, col);
-        }
-    }
+    let (width, height) = (2048, 2048);
+    let img = ImageBuffer::from_fn(width, height, |x, y| {
+        let re = lerp(
+            0.281717921930774,
+            0.281717921930775,
+            x as f64 / width as f64,
+        );
+        let im = lerp(
+            0.5771052841488504,
+            0.5771052841488506,
+            y as f64 / height as f64,
+        );
+        let c = Complex::new(re, im);
+        let escape_count = c.escape_count(Complex::id(), 8., 1000);
+        let frac = escape_count as f64 / 200. * 256.;
+        Rgb([frac as u8, frac as u8, frac as u8])
+    });
     img.save("penis.png").unwrap();
 }
 
